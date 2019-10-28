774ab4cdb69176647df83189999eb46451c84548
Aurélien <aurelien.spinelli@hotmail.fr>
2019-10-28 15:56:55 +0100

    test all combinations

--- test/board_test.go
+++ test/board_test.go
@@ -77,3 +77,61 @@ func TestWinByLaneRightToLeft_2(t *testing.T) {
 		log.Fatal(err)
 	}
 }
+
+func TestWinByLaneTopToBot_1(t *testing.T) {
+	var s string
+	var err error
+	b = game.NewBoard(3, 3)
+	boardSquare := []string{"x", "-", "-", "x", "-", "-", "x", "-", "-"}
+	err = b.Fill(boardSquare)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// INIT OK
+
+	s, err = b.ToString()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Print(s)
+	}
+
+	assert.True(t, b.IsWinByLineTopToBot(), "Should be true")
+
+}
+
+func TestWinByLaneTopToBot_2(t *testing.T) {
+	var numberOfWin int = 0
+	file, err := os.Open("./combination/all_combinations.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var line []string
+		line = strings.Split(scanner.Text(), "")
+
+		b = game.NewBoard(3, 3)
+		err = b.Fill(line)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// INIT OK
+
+		if b.IsWinByLineTopToBot() {
+			numberOfWin += 1
+		}
+		// s, err = b.ToString()
+		// if err != nil {
+		// 	fmt.Println(err)
+		// } else {
+		// 	fmt.Println(s)
+		// }
+	}
+	fmt.Println("Number of win : ", numberOfWin)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
